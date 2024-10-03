bc9683a2674133be1a78362a65fda79c5283ec36
Paulo Henrique <paulo.carvalho.1524995@sga.pucminas.br>
2024-10-03 01:51:11 -0300

    .Inicio criação da tela Liturgia Pequenos ajustes na _Layout.cshtml

--- src/PortalCatolicoBrasil/obj/Debug/net8.0/PortalCatolicoBrasil.AssemblyInfo.cs
+++ src/PortalCatolicoBrasil/obj/Debug/net8.0/PortalCatolicoBrasil.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PortalCatolicoBrasil")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c79fbd9aec23d9517dca58510b1746d2d9dfc31f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d691deac263ea7f4f0ff46233533560cd0d250fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("PortalCatolicoBrasil")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PortalCatolicoBrasil")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/PortalCatolicoBrasil/wwwroot/js/site.js
+++ src/PortalCatolicoBrasil/wwwroot/js/site.js
@@ -1,4 +1,33 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿/*-----------------INÍCIO LITURGIA-----------------*/
 
-// Write your JavaScript code.
+const buttons = document.querySelectorAll(".btn--group button");
+buttons.forEach(button => {
+    button.addEventListener("click", function () {
+        buttons.forEach(btn => btn.classList.remove("active"));
+        this.classList.add("active")
+    });
+});
+
+async function obterLiturgia() {
+    const container = document.getElementById('liturgia-container');
+    container.classList.add('liturgia--text');
+    container.innerHTML = '<p>Carregando...</p>';
+
+    try {
+        const response = await fetch('/liturgia');
+        if (!response.ok) {
+            throw new Error('Erro ao buscar a liturgia');
+        }
+        const data = await response.text();
+        container.innerHTML = data;
+    } catch (error) {
+        console.error('Erro:', error);
+        container.innerHTML = '<p>Erro ao carregar a liturgia.</p>';
+    }
+}
+
+window.onload = function () {
+    obterLiturgia();
+};
+
+/*-----------------FIM LITURGIA-----------------*/
